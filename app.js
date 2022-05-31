da63e4e85e9caf4485b9c959a0efc9968e2639bc
vino04003 <89254939+vino04003@users.noreply.github.com>
2022-05-31 15:55:44 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,97 @@
+const statusDisplay = document.querySelector('.Spel-status'); 
+ 
+//Vi skapar möjligheter, hur många "boxar" det ska vara och vad spelaren har för värde "X"
+let gameActive = true;
+let NuvarandeSpelare = "X";
+let gameState = ["", "", "", "", "", "", "", "", ""];
+ 
+//Meddelanden vid slutet av omgång och under omgång
+const winningMessage = () => `Grattis ${NuvarandeSpelare} Du Vann!`;
+const drawMessage = () => `Spelet slutade oavgjort!`;
+const NuvarandeSpelareTurn = () => `Det är "${NuvarandeSpelare}" tur`;
+ 
+statusDisplay.innerHTML = NuvarandeSpelareTurn();
+ 
+//Vinst möjligheterna
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+//Funktionen för att man ska kunna klicka
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    gameState[clickedCellIndex] = NuvarandeSpelare;
+    clickedCell.innerHTML = NuvarandeSpelare;
+}
+ 
+//Spel "pjäserna", och vems tur det är
+function handlePlayerChange() {
+    NuvarandeSpelare = NuvarandeSpelare === "X" ? "O" : "X";
+    statusDisplay.innerHTML = NuvarandeSpelareTurn();
+}
+
+//Funktionen för att man ska kunna vinna
+function handleResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break
+        }
+    }
+
+    if (roundWon) {
+        statusDisplay.innerHTML = winningMessage();
+        gameActive = false;
+        return;
+    }
+ 
+    let roundDraw = !gameState.includes("");
+    if (roundDraw) {
+        statusDisplay.innerHTML = drawMessage();
+        gameActive = false;
+        return;
+    }
+ 
+    handlePlayerChange();
+}
+ 
+//Vad som sker när man klickar 
+function handleCellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
+ 
+    if (gameState[clickedCellIndex] !== "" || !gameActive) {
+        return;
+    }
+ 
+    handleCellPlayed(clickedCell, clickedCellIndex);
+    handleResultValidation();
+}
+ 
+//Vad som sker vid omstart
+function SpelOmstart() {
+    gameActive = true;
+    NuvarandeSpelare = "X";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    statusDisplay.innerHTML = NuvarandeSpelareTurn();
+    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
+}
+ 
+ 
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.querySelector('.Spel-omstart').addEventListener('click', SpelOmstart);
+
